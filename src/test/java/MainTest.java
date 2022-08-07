44fe904708a6e41ec141c3bcfc5d66deda1f54b6
sezergemtsov <sezer.gemtsov@ya.ru>
2022-08-07 16:58:08 +0300

    migration to Hamcrest

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,16 +1,18 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class MainTest {
     @Test
     public void binaryTreeTest() {
         //arrange
         final int[] exampleArr = {10, 11, 12};
-        final int[][] expectedArr = {{0,11,0},{10,0,12}};
+        final int[][] expectedArr = {{0, 11, 0}, {10, 0, 12}};
         //act
         int[][] test = Main.binaryTree(exampleArr);
         //assert
-        Assertions.assertEquals(test,expectedArr);
+        assertThat(test, equalTo(expectedArr));
     }
 
     @Test
@@ -18,22 +20,27 @@ public class MainTest {
         //arrange
         final int[] exampleArr = {10, 11, 12};
         final int[][] modifiedArr = new int[2][3];
-        final int[][] expectedArr ={{0,11,0},{10,0,12}};
+        final int[][] expectedArr = {{0, 11, 0}, {10, 0, 12}};
         //act
-        int[][] result = Main.mark(exampleArr,modifiedArr,0,2,0,2);
+        int[][] result = Main.mark(exampleArr, modifiedArr, 0, 2, 0, 2);
         //assert
-        Assertions.assertArrayEquals(expectedArr,result);
+        assertThat(exampleArr, equalTo(result));
     }
 
     @Test
     public void printArr() {
         //arrange
-        final int[][] exampleArr = {{0,11,0},{10, 0, 12}};
+        final int[][] exampleArr = {{0, 11, 0}, {10, 0, 12}};
         final String expectedResult = "  11  \n10  12";
         //act
         String result = Main.printArr(exampleArr);
         //assert
-        Assertions.assertEquals(expectedResult,result);
+        assertThat(expectedResult, equalToIgnoringWhiteSpace(expectedResult));
+
+        assertThat(result, startsWith(" "));
+        assertThat(result, allOf(containsString("11"), containsString("\n"), endsWith("12")));
+        assertThat(result, not(endsWith(" ")));
+
     }
 
 }
